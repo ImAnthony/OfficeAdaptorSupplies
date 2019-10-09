b88e8bda2c84cd5afd5438a10edc923c5255a2a5
Anthony <anthonyisnot2@gmail.com>
2019-10-09 12:11:23 -0700

    Basic GUI
    Laid out basic gui and stuff and class files

---
+++ AssetManagment/AssetManagment/CheckedOutItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetManagment
+{
+    public class CheckedOutItem
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string reason { get; set; }
+        public string LoandedDate { get; set; }
+        public string AdapterName { get; set; }
+        
+        
+    }
+
+}
